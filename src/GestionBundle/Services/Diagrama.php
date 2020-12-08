96411de01444b820a47411ed670a39030004003d
= <leochabur@gmail.com>
2020-12-08 18:17:49 -0300

    ok

--- src/GestionBundle/Services/Diagrama.php
+++ src/GestionBundle/Services/Diagrama.php
@@ -9,7 +9,7 @@ class Diagrama {
         $orden->setFecha($fecha);
         $salida = \DateTime::createFromFormat('Y-m-dH:i:s', $fecha->format('Y-m-d').$turno->getHoraInicial()->format('H:i:s'));
         $llegada = clone $salida;
-        $llegada->add(new \DateInterval('PT'.$turno->getDuracion()->format('H').'H'.$turno->getDuracion()->format('m').'M'));
+        $llegada->add(new \DateInterval('PT'.$turno->getDuracion()->format('H').'H'.$turno->getDuracion()->format('i').'M'));
         $orden->setSalida($salida);
         $orden->setLlegada($llegada);
         $orden->setTurno($turno);
